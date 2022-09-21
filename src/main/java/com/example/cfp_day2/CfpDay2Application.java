a6d04146c20a46487f0ddb8a34d897b038570273
champ09D <darshanmhatre709@gmail.com>
2022-09-21 17:09:04 +0530

    Added Create Hello World Spring App File Case Add

---
+++ src/main/java/com/example/cfp_day2/CfpDay2Application.java
@@ -0,0 +1,14 @@
+package com.example.cfp_day2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfpDay2Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        SpringApplication.run(CfpDay2Application.class, args);
+    }
+
+}
